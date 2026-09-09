1fccfc14bf29c3a1d7febb8078d1d4c1cae3e808
JoasBlok <joasblok@gmail.com>
2026-09-09 20:32:41 +1000

    Interesting 2D object being drawn

--- Assets/Scripts/A1/IGB283TriangleSpawner.cs
+++ Assets/Scripts/A1/IGB283TriangleSpawner.cs
@@ -1,108 +1,109 @@
+using System.Collections.Generic;
 using UnityEngine;
-
 
 public class IGB283TriangleSpawner : MonoBehaviour
 {
-    public GameObject trianglePrefab;
-    [SerializeField] private float triangleCount;
     [SerializeField] private Material material;
     [SerializeField] private Color color = Color.white;
+
     private Mesh mesh;
 
+    // The 9 vertices that make up one diamond
     IGB283Vector3[] diamondVertices =
-{
-    new IGB283Vector3(0, 4, 0),   // 0 centre
-    new IGB283Vector3(0, 0, 0),   // 1 bottom
-    new IGB283Vector3(1, 2, 0),   // 2
-    new IGB283Vector3(2, 4, 0),   // 3 right
-    new IGB283Vector3(1, 6, 0),   // 4
-    new IGB283Vector3(0, 8, 0),   // 5 top
-    new IGB283Vector3(-1, 6, 0),  // 6
-    new IGB283Vector3(-2, 4, 0),  // 7 left
-    new IGB283Vector3(-1, 2, 0)   // 8
-};
+    {
+        new IGB283Vector3(0, 4, 0),   // 0 centre
+        new IGB283Vector3(0, 0, 0),    // 1 bottom
+        new IGB283Vector3(1, 2, 0),    // 2 lower-right
+        new IGB283Vector3(2, 4, 0),    // 3 right
+        new IGB283Vector3(1, 6, 0),    // 4 upper-right
+        new IGB283Vector3(0, 8, 0),    // 5 top
+        new IGB283Vector3(-1, 6, 0),   // 6 upper-left
+        new IGB283Vector3(-2, 4, 0),   // 7 left
+        new IGB283Vector3(-1, 2, 0)    // 8 lower-left
+    };
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Add a mesh filter and mesh renderer, getting and setting useful variables
+        // Create the mesh
         mesh = gameObject.AddComponent<MeshFilter>().mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
-        // Clear all vertex and index data from the mesh
+
         mesh.Clear();
 
-        Matrix3x3 rotation = Matrix3x3.RotationZ(90);
-        IGB283Vector3 result =
-            rotation.MultiplyVector3(new IGB283Vector3(0, 8, 0));
+        // Lists to hold the entire star
+        List<IGB283Vector3> vertices = new List<IGB283Vector3>();
+        List<int> triangles = new List<int>();
 
-        Debug.Log(result);
-
-
-
-        //for (int i = 0; i < 4; i++)
-        //{
-        //    float angle = i * 90f;
-
-        //    Matrix3x3 rotation = Matrix3x3.RotationZ(angle);
-
-        //    for (int j = 0; j < diamondVertices.Length; j++)
-        //    {
-        //        IGB283Vector3 rotated =
-        //            rotation.MultiplyVector3(diamondVertices[j]);
-
-        //        // Add rotated to your mesh vertices
-        //    }
-        //}
-
-
-        //IGB283Vector3[] vertices = IGB283Triangle.MakeTriangle(v1, v2, v3);
-        mesh.vertices = IGB283Vector3.ConvertTo(diamondVertices);
-
-        mesh.colors = new Color[] { color, color, color, color, color, color, color, color, color };
-        mesh.triangles = new int[]
+        // Create the four diamonds
+        for (int i = 0; i < 4; i++)
         {
-            0, 5, 6,  // top-left
-            0, 6, 7,  // upper-left
-            0, 7, 8,  // lower-left
-            0, 8, 1,  // bottom-left
+            float angle = i * 90f;
 
-            0, 1, 2,  // bottom-right
-            0, 2, 3,  // lower-right
-            0, 3, 4,  // upper-right
-            0, 4, 5   // top-right`
-        };
+            Matrix3x3 rotation = Matrix3x3.RotationZ(angle);
 
+            // Remember where this diamond's vertices start
+            int vertexOffset = vertices.Count;
 
+            // Rotate every vertex of the diamond
+            for (int j = 0; j < diamondVertices.Length; j++)
+            {
+                IGB283Vector3 rotated =
+                    rotation.MultiplyVector3(diamondVertices[j]);
 
+                vertices.Add(rotated);
+            }
 
+            // Add the 8 triangles for this diamond
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 5);
+            triangles.Add(vertexOffset + 6);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 6);
+            triangles.Add(vertexOffset + 7);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 7);
+            triangles.Add(vertexOffset + 8);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 8);
+            triangles.Add(vertexOffset + 1);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 1);
+            triangles.Add(vertexOffset + 2);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 2);
+            triangles.Add(vertexOffset + 3);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 3);
+            triangles.Add(vertexOffset + 4);
+
+            triangles.Add(vertexOffset + 0);
+            triangles.Add(vertexOffset + 4);
+            triangles.Add(vertexOffset + 5);
+        }
+
+        // Convert our custom vertices to Unity vertices
+        mesh.vertices = IGB283Vector3.ConvertTo(vertices.ToArray());
+
+        // Give every vertex a colour
+        Color[] colors = new Color[vertices.Count];
+
+        for (int i = 0; i < colors.Length; i++)
+        {
+            colors[i] = color;
+        }
+
+        mesh.colors = colors;
+
+        // Set triangle indices
+        mesh.triangles = triangles.ToArray();
+
+        // Optional: recalculate the mesh bounds
+        mesh.RecalculateBounds();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //void CreateObject()
-    //{ 
-    //    List<IGB283Vector3> vertices  = new List<IGB283Vector3>();
-
-    //    for (int i = 0; i< 32; i++)
-    //    {
-    //        IGB283Vector3[] triangle = SpawnTriangle(i);
-    //        vertices.Add(triangle[0]);
-    //        vertices.Add(triangle[1]);
-    //        vertices.Add(triangle[2]);
-    //    }
-    //}
-
-    //IGB283Vector3[] SpawnTriangle(int i)
-    //{
-        
-
-
-    //    return trianglePrefab.GetComponent<IGB283Triangle>().MakeTriangle(v1, v2, v3);
-    //}
-
 }
