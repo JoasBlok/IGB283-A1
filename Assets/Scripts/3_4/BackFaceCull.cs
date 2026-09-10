3aaa7435af2717d6aa55f63cad2e10adfd2a3cf7
JoasBlok <joasblok@gmail.com>
2026-09-10 16:33:06 +1000

    Workshop 3_4

---
+++ Assets/Scripts/3_4/BackFaceCull.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackFaceCull : MonoBehaviour
+{
+    private Mesh mesh;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        CullBackFaces();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CullBackFaces() {
+        List<int> triangles = new List<int>();
+
+        for (int i = 0; i < mesh.triangles.Length; i = i + 3) {
+        // Get the vertices of the current triangle
+            Vector3 v0 = mesh.vertices[mesh.triangles[i + 0]];
+            Vector3 v1 = mesh.vertices[mesh.triangles[i + 1]];
+            Vector3 v2 = mesh.vertices[mesh.triangles[i + 2]];
+
+            // Create displacement vectors representing the trianglefrom point v0
+            Vector3 s0 = v1 - v0;
+            Vector3 s1 = v2 - v0;
+
+            // Find the triangle's normal
+            Vector3 normal = Vector3.Cross(s1, s0);
+            Vector3 rotatedNormal = transform.rotation * normal;
+
+            // Find the dot product with the view direction
+            Vector3 viewDirection = Camera.main.transform.forward;
+            float dotProduct = Vector3.Dot(viewDirection, rotatedNormal);
+
+            // If the face is visible, add the indices to the new list
+            if (dotProduct > 0f)
+            {
+                triangles.Add(mesh.triangles[i + 0]);
+                triangles.Add(mesh.triangles[i + 1]);
+                triangles.Add(mesh.triangles[i + 2]);
+            }
+        }
+        // Replace the mesh's triangles
+        mesh.triangles = triangles.ToArray();
+    }
+}
